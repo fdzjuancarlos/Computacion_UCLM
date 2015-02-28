e1a1a33ab3e5f32bb3dc1c08e0c9790b56ff5680
fdzjuancarlos <fdz.juancarlos@gmail.com>
2015-02-28 16:23:48 +0100

    First Exercise

--- Declarativa/Practica1/biblia_jc.pl
+++ Declarativa/Practica1/biblia_jc.pl
@@ -62,4 +62,31 @@ hijo(X,Y) :- hombre(X), ascendiente_directo(Y,X).
 hija(X,Y) :- mujer(X), ascendiente_directo(Y,X).
 
 
+
+
 abuelo(X,Y) :- hombre(X) , ascendiente_directo(X,Z) , ascendiente_directo(Z,Y).
+abuela(X,Y) :- mujer(X) , ascendiente_directo(X,Z) , ascendiente_directo(Z,Y).
+
+hermano(X,Y) :- hombre(X), ascendiente_directo(Z,X), ascendiente_directo(Z,Y), dif(X,Y).  
+hermana(X,Y) :- mujer(X), ascendiente_directo(X,Z), ascendiente_directo(Y,Z), dif(X,Y).  
+
+tio(X,Y) :- hombre(X), hermano(X,Z), ascendiente_directo(Z,Y).
+tia(X,Y) :- mujer(X), hermano(X,Z), ascendiente_directo(Z,Y).
+
+sobrino(X,Y) :- tio(Y,X) ; tia(Y,X) , hombre(X).
+sobrina(X,Y) :- tio(Y,X) ; tia(Y,X) , mujer(X).
+
+primo(X,Y) :- (tio(Z,X);tia(Z,X)),ascendiente_directo(Z,Y) , hombre(X).
+prima(X,Y) :- (tio(Z,X);tia(Z,X)),ascendiente_directo(Z,Y) , mujer(X).
+
+parentesco_familiar(X,Y) :- (ascendiente(X,Y);hermano(X,Y);hermana(X,Y);
+tio(X,Y);tia(X,Y);sobrino(X,Y);sobrina(X,Y);primo(X,Y);prima(X,Y)).
+
+incestuosos(X,Y) :- parentesco_familiar(X,Y) , ascendiente_directo(X,Z),
+ascendiente_directo(Y,Z).
+
+%Ejercicio 2
+
+
+
+
