54175b6c81efa54aa0fbb81745cefdb19e25c3af
gualdras <victor.gualdras@gmail.com>
2015-03-01 13:02:57 +0100

    p1 y p2 declarativa

--- Declarativa/p1_victor.pl
+++ Declarativa/p1_victor.pl
@@ -3,7 +3,6 @@
 padre(abraham,isaac).
 padre(abraham,ismael).
 
-
 padre(haran,lot).
 padre(haran,melca).
 padre(haran,jesca).
@@ -16,12 +15,10 @@ padre(teraj,sara).
 padre(isaac,esau).
 padre(isaac,jacob).
 
-padre(najor,batuel).
-
 padre(batuel,rebeca).
 padre(batuel,laban).
 
-
+padre(najor,batuel).
 
 
 
@@ -30,7 +27,6 @@ madre(sara,isaac).
 madre(rebeca,esau).
 madre(rebeca,jacob).
 madre(melca,batuel).
-
 
 
 hombre(isaac).
@@ -75,7 +71,7 @@ abuela(X, Z) :- (mujer(X), ascendiente_directo(X, Y), ascendiente_directo(Y, Z))
 hermano(X, Y) :- (hombre(X), ascendiente_directo(Z, X), ascendiente_directo(Z, Y)).
 hermana(X, Y) :- (mujer(X), ascendiente_directo(Z, X), ascendiente_directo(Z, Y)).
 
-tio(X, Y) :- (ascendiente_directo(Z, Y), hermano(X, Z)).
+tio(X, Y) :- (ascendiente_directo(Z, Y), (hermano(X, Z); (casado(X, T), hermano(T, Z))).
 tia(X, Y) :- (ascendiente_directo(Z, Y), hermana(X, Z)).
 
 sobrino(X, Y) :- hombre(X), (tio(Y,X); tia(Y,X)).
@@ -84,8 +80,8 @@ sobrina(X, Y) :- mujer(X), (tio(Y,X); tia(Y,X)).
 primo(X, Y) :- hombre(X), (tio(Z, X); tia(Z, X)), (padre(Z, Y); madre(Z, Y)).
 prima(X, Y) :- mujer(X), (tio(Z, X); tia(Z, X)), (padre(Z, Y); madre(Z, Y)).
 
-familia(X, Y) :- (ascendiente(X, Y); descendiente(X, Y); hermano(X, Y); hermana(X, Y); primo(X, Y); prima(X, Y)).
+familia(X, Y) :- (ascendiente_directo(X, Y); descendiente_directo(X, Y); abuelo(X,Y); abuelo(Y,X); abuela(X,Y); abuela(Y,X); hermano(X, Y); hermana(X, Y); primo(X, Y); prima(X, Y); tio(X,Y); tio(Y,X); tia(X,Y), tia(Y,X)).
 
-incesto(X, Y) :- casado(X, Y), familia(X, Y).
+incestuosos(X, Y) :- casado(X, Y), familia(X, Y).
 
 
