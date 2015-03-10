da25ebaea9a212ab652e80fc12ef419e08db90c8
gualdras <victor.gualdras@gmail.com>
2015-03-10 18:08:23 +0100

    Ej13 Declarativa

--- Declarativa/Victor/practica3/Gualdras_delaCruz_Lab3.pl
+++ Declarativa/Victor/practica3/Gualdras_delaCruz_Lab3.pl
@@ -13,3 +13,19 @@ partir3(X, [P|Rp], L1, [P|Rq]) :- P>X, partir3(X,Rp,L1, Rq).
 
 misus(X, [X|Rx], Rx) :- !.
 misus(X, [Z|Rx], [Z|Ry]) :- misus(X, Rx, Ry).
+
+%Ejercicio 11
+
+
+%merge_sort(Lista, ListaOrdenada) :- merge(Lista, ListaOrdenada).
+
+%Fallooo
+merge_sort(L, L) :- length(L,1), !.
+merge_sort(L, O) :- mitad(L, L1, L2), merge_sort(L1, P), merge_sort(L2,Q), ordena(P, Q, O).
+
+ordena([], X, X) :- !.
+ordena(X, [], X) :- !.
+ordena([X|Rx], [Y|Ry], [X|R]) :- X=<Y, ordena(Rx, [Y|Ry], R), !.
+ordena([X|Rx], [Y|Ry], [Y|R]) :- X>Y, ordena([X|Rx], Ry, R), !.
+
+mitad(L, L1, L2) :- length(L, To), T1 is To//2, length(L1,T1), append(L1,L2,L).
