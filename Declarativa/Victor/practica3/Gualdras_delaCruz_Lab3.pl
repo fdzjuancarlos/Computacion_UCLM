6763b6d15c969ac40e09037073e24a4a9f867b03
gualdras <victor.gualdras@gmail.com>
2015-03-11 13:01:40 +0100

    P3 declarativa

--- Declarativa/Victor/practica3/Gualdras_delaCruz_Lab3.pl
+++ Declarativa/Victor/practica3/Gualdras_delaCruz_Lab3.pl
@@ -17,9 +17,7 @@ misus(X, [Z|Rx], [Z|Ry]) :- misus(X, Rx, Ry).
 %Ejercicio 11
 
 
-%merge_sort(Lista, ListaOrdenada) :- merge(Lista, ListaOrdenada).
 
-%Fallooo
 merge_sort(L, L) :- length(L,1), !.
 merge_sort(L, O) :- mitad(L, L1, L2), merge_sort(L1, P), merge_sort(L2,Q), ordena(P, Q, O).
 
@@ -29,3 +27,45 @@ ordena([X|Rx], [Y|Ry], [X|R]) :- X=<Y, ordena(Rx, [Y|Ry], R), !.
 ordena([X|Rx], [Y|Ry], [Y|R]) :- X>Y, ordena([X|Rx], Ry, R), !.
 
 mitad(L, L1, L2) :- length(L, To), T1 is To//2, length(L1,T1), append(L1,L2,L).
+
+%Ejercicio 13
+
+monomio(C):- integer(C).
+monomio(x).
+monomio(C*x):- integer(C).
+monomio(C*x**E) :- integer(C), integer(E).
+polinomio(M+P) :- monomio(M), monomio(P).
+polinomio(M-P) :- monomio(M), monomio(P).
+polinomio(M+P) :- monomio(M), polinomio(P).
+polinomio(M+P) :- polinomio(M), monomio(P).
+
+
+d(C, 0) :- integer(C).
+d(C*x, D) :- D is C.
+
+d(C*x**E, D) :- D = C1*x, C1 is C*E, E1 is E-1, E1 =:= 1.
+d(C*x**E, D) :- D = C1*x**E1, C1 is C*E, E1 is E-1.
+
+d(P, D) :- polinomio(P) = polinomio(X+Y), d(X,D1), d(Y,D2), D = D1, D2 =:= 0, !.
+d(P, D) :- polinomio(P) = polinomio(X+Y), d(X,D1), d(Y,D2), D = D1+D2.
+
+d(P, D) :- polinomio(P) = polinomio(X-Y), d(X,D1), d(Y,D2), D = D1, D2 =:= 0, !.
+d(P, D) :- polinomio(P) = polinomio(X-Y), d(X,D1), d(Y,D2), D = D1-D2.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
