bd3a7e8a6138951c8ea22837ce3a7ee753d92a70
fdzjuancarlos <fdz.juancarlos@gmail.com>
2015-03-29 03:19:39 +0200

    Prolog 5

---
+++ Declarativa/Leonard/Practica4/FernandezDuran_Lab4.pl
@@ -0,0 +1,88 @@
+%trace, esListaRosadelfas(nodo(_,[nodo(_,[hoja(_), hoja(_)])),nodo(_,[nodo(_,[hoja(_),hoja(_)])])).
+
+%Ejercicio 14
+% esRosadelfa(O), el objeto O es una rosadelfa.
+
+esRosadelfa(nil).
+esRosadelfa(hoja(_)).
+esRosadelfa(nodo(_, Rosadelfas)) :- esListaRosadelfas(Rosadelfas).
+
+esListaRosadelfas([R]) :- esRosadelfa(R).
+esListaRosadelfas([R|Rosadelfas]) :- esRosadelfa(R), esListaRosadelfas(Rosadelfas).
+
+%a
+esRosadelfa(nil,N):- N is 0.
+esRosadelfa(hoja(_), N):- N is 1.
+esRosadelfa(nodo(_,Rosadelfas), N) :- esListaRosadelfas(Rosadelfas, N1), length(Rosadelfas, N2), mayor(N1,N2,N). 
+
+esListaRosadelfas([R], N) :- esRosadelfa(R,N).
+esListaRosadelfas([R|Rosadelfas], N) :- esRosadelfa(R,N1), esListaRosadelfas(Rosadelfas,N2), mayor(N1,N2,N).
+
+mayor(N1,N2,N) :- N1>=N2, N is N1. 
+mayor(N1,N2,N) :- N1<N2, N is N2.
+
+%Ejercicio15
+
+%a
+peso(nil, P):- P is 0.
+peso(hoja(_), P):- P is 1.
+peso(nodo(_,Rosadelfas), P) :- pesoLista(Rosadelfas, X), P is  X+1.
+
+pesoLista([R], P) :- peso(R,P).
+pesoLista([R|Rosadelfas], P) :- peso(R,P1), pesoLista(Rosadelfas,P2), P is P1+P2.
+
+%b
+grado(A, G) :- esRosadelfa(A,G).
+
+%c
+frontera(nil,N):- N=[].
+frontera(hoja(X), N):- N=[hoja(X)].
+frontera(nodo(_,Rosadelfas), N) :- esListaFrontera(Rosadelfas, N). 
+
+esListaFrontera([R], F):- frontera(R,F).
+esListaFrontera([R|Rosadelfas], N) :- frontera(R,N1), esListaFrontera(Rosadelfas,N2), append(N1,N2,N).
+
+
+%d
+
+preorden(nil, []).
+preorden(hoja(X), P) :- P = [hoja(X)].
+
+%preorden(nodo(X,Rosadelfas), Z) :- esListaPreorden(Rosadelfas, R), aplanar([nodo(X)|R], Z).
+preorden(nodo(X,Rosadelfas), [nodo(X)|R]) :- esListaPreorden(Rosadelfas, R). 
+
+esListaPreorden([R], F):- preorden(R,F).
+esListaPreorden([R|Rosadelfas], Z) :- preorden(R,X), esListaPreorden(Rosadelfas,Rx), append(X,Rx,Z).
+
+
+%Ejercicio 16
+
+% construirRosadelfa(L, G, R), construye una rosadelfa R, a partir de los elementos de una lista L.
+construirRosadelfa([], _, nil).
+construirRosadelfa([X], _, hoja(X)).
+construirRosadelfa([X|L], G, nodo(X, [R|Rosadelfas])):- 
+partir(L, G, [L1|GListas]),
+construirRosadelfa(L1, G, R),
+construirRosadelfas(GListas, G, Rosadelfas).
+
+% inspecciona una lista de listas y por cada lista inspeccionada crea una rosadelfa
+construirRosadelfas([], _, []).
+construirRosadelfas([L|GListas], G, [R|Rosadelfas]) :-
+construirRosadelfa(L, G, R),
+construirRosadelfas(GListas, G, Rosadelfas).
+% partir(L, G, [L1|GListas]), divide la lista L en una secuencia de listas de longitud 
+% menor o igual que G, que se devuelve como una lista de listas en el segundo argumento.
+partir(L, G, [L]):- length(L,N), N < G.
+partir(L, 0, L).
+partir(L, G, [L1|GListas]):- length(L,N), N >= G,
+G>0,
+NuevaLength is N//G,
+length(L1, NuevaLength),
+append(L1, LL, L),
+PorPartir is G-1,
+partir(LL, PorPartir, GListas).
+
+
+
+
+
