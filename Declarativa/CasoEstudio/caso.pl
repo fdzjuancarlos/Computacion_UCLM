519a3649b7ff79265902c48237725d52d31298f5
gualdras <victor.gualdras@gmail.com>
2015-04-27 01:43:33 +0200

    resolutor terminado

--- Declarativa/CasoEstudio/caso.pl
+++ Declarativa/CasoEstudio/caso.pl
@@ -17,6 +17,8 @@ clausula(not(p);r).
 %clausula(not(p)).
 %clausula(p;q).
 
+% Resolutor hace la parte principal de aplicar el mecanismo de resolucion lineal. Usa reglas auxiliares.
+
 resolutor:- clausula(X), resolutor(X, []), !.
 resolutor(X, L):- clausula(Y), (resolutor(X,Y,L); resolutor(Y,X,L)).
 resulutor(X, L):- recorrer(X, L, L).
@@ -37,50 +39,34 @@ resolutor((Y;X), (not(X);Z), L):- repeticion(Y, Z, C), (clausula(C); esta(C, L))
 resolutor((Y;X), (Z;not(X)), L):- repeticion(Y, Z, C), (clausula(C); esta(C, L)), !, fail; resolutor(C, [L|C]).
 
 
-resolutor((X;Y;Z), not(X), L):- 
-resolutor((Y;X;Z), not(X), L):-
-resolutor((Y;Z;X), not(X), L):-
-
-resolutor((not(X);Y;Z), X, L):- 
-resolutor((Y;not(X);Z), X, L):-
-resolutor((Y;Z;not(X)), X, L):-
+resolutor((Y;X;Z), not(X), L):- (clausula(Y); esta(Y, L)), !, fail; resolutor(Y, [L|Y]).
+resolutor((Y;not(X);Z), X, L):- (clausula(Y); esta(Y, L)), !, fail; resolutor(Y, [L|Y]).
 
 
-resolutor((X;Y;Z), (not(X); M), L):- 
-resolutor((Y;X;Z), (not(X); M), L):-
-resolutor((Y;Z;X), (not(X); M), L):-
+resolutor((Y;X;Z), (not(X); M), L):- repeticion((Y;Z), M, C), (clausula(C); esta(C, L)), !, fail; resolutor(C, [L|C]).
+resolutor((Y;X;Z), (M; not(X)), L):- repeticion((Y;Z), M, C), (clausula(C); esta(C, L)), !, fail; resolutor(C, [L|C]).
 
-resolutor((X;Y;Z), (M; not(X)), L):- 
-resolutor((Y;X;Z), (M; not(X)), L):-
-resolutor((Y;Z;X), (M; not(X)), L):-
-
-resolutor((not(X);Y;Z), (X; M), L):- 
-resolutor((Y;not(X);Z), (X; M), L):-
-resolutor((Y;Z;not(X)), (X; M), L):-
-
-resolutor((not(X);Y;Z), (M; X), L):- 
-resolutor((Y;not(X);Z), (M; X), L):-
-resolutor((Y;Z;not(X)), (M; X), L):-
+resolutor((Y;not(X);Z), (X; M), L):- repeticion((Y;Z), M, C), (clausula(C); esta(C, L)), !, fail; resolutor(C, [L|C]).
+resolutor((Y;not(X);Z), (M; X), L):- repeticion((Y;Z), M, C), (clausula(C); esta(C, L)), !, fail; resolutor(C, [L|C]).
 
 
-resolutor((X;Y;Z), (not(X); M; N), L):- 
-resolutor((Y;X;Z), (not(X); M; N), L):-
-resolutor((Y;Z;X), (not(X); M; N), L):-
+resolutor((Y;X;Z), (not(X); M; N), L):- repeticion((Y;Z), (M;N), C), (clausula(C); esta(C, L)), !, fail; resolutor(C, [L|C]).
 
-resolutor((X;Y;Z), (M; not(X); N), L):- 
-resolutor((Y;X;Z), (M; not(X); N), L):-
-resolutor((Y;Z;X), (M; not(X); N), L):-
+resolutor((X;Y;Z), (M; not(X); N), L):- repeticion((Y;Z), (M;N), C), (clausula(C); esta(C, L)), !, fail; resolutor(C, [L|C]).
+resolutor((Y;X;Z), (M; not(X); N), L):- repeticion((Y;Z), (M;N), C), (clausula(C); esta(C, L)), !, fail; resolutor(C, [L|C]).
+resolutor((Y;Z;X), (M; not(X); N), L):- repeticion((Y;Z), (M;N), C), (clausula(C); esta(C, L)), !, fail; resolutor(C, [L|C]).
 
-resolutor((not(X);Y;Z), (M; N; X), L):- 
-resolutor((Y;not(X);Z), (M; N; X), L):-
-resolutor((Y;Z;not(X)), (M; N; X), L):-
+resolutor((Y;X;Z), (M; N; not(X)), L):- repeticion((Y;Z), (M;N), C), (clausula(C); esta(C, L)), !, fail; resolutor(C, [L|C]).
 
 
-recorrer(_, [], _):- false.
+% Recorrer recorre la lista de .......
+
+(_, [], _):- false.
 recorrer(X, [Y|R], L):- resolutor(X,Y,L); resolutor(Y,X,L); recorrer(X,R,L).
 
+% Repeticion se asegura de que no se incluyen clausulas del tipo (p;p;q).
+
 repeticion(X,X,X).
-repeticion(X,Y,(X;Y)).
 
 repeticion((X;Y), X, Y). 
 repeticion((Y;X), X, Y).
@@ -88,6 +74,10 @@ repeticion((Y;X), X, Y).
 repeticion((X;Y), (X;Z), S):- ((repeticion(Y,Z,S1); repeticion(Z,Y,S1)), S=(X;S1)) ; S=(X;Y;Z).
 repeticion((X;Y), (Z;X), S):- ((repeticion(Y,Z,S1); repeticion(Z,Y,S1)), S=(X;S1)) ; S=(X;Y;Z).
 repeticion((Y;X), (Z;X), S):- ((repeticion(Y,Z,S1); repeticion(Z,Y,S1)), S=(X;S1)) ; S=(X;Y;Z).
+
+repeticion(X,Y,(X;Y)).
+
+% Esta comprueba que una clausula esta ya añadida a la lista de .........
 
 esta(_, []):- false.
 esta(X, [X|_]):-!.
